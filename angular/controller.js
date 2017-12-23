33d4a6297bbe093d60fc1b8ce44a0c3e501a245f
ArcGate-Satyendra <archit@stonex>
2017-12-23 19:31:30 +0530

    adding more files

--- angular/controller.js
+++ angular/controller.js
@@ -2,12 +2,24 @@ var app = angular.module("myApp", ['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
     .when('/', {
-        template : 'Welcome User !'
+        templateUrl : 'login.html'
     })
-    .when('/anotherPage', {
-        template : 'Welcome User Again!'
+    .when('/dashboard', {
+        template : 'dashboard.html'
     })
     .otherwise({
         redirectTo : '/'
     })
+})
+
+app.controller('loginCtrl', function($scope, $location){
+    $scope.submit = function(){
+        var uname = $scope.username;
+        var password = $scope.password;
+        if($scope.username=="admin" && $scope.username!="" && $scope.password=="admin" && $scope.password != ""){
+            $location.path('dashboard');
+        }else{
+            alert("Wrong Stuff");
+        }
+    }
 })
